35d5edecca0007c9c72b605bb10912ce3b0f83ae
Md. Shaiful Islam <islambuet@gmail.com>
2022-10-20 05:23:07 +0600

    Crops Task Completed

---
+++ src/tasks/setup/crops/labels.js
@@ -0,0 +1,26 @@
+export default {
+    label_task:{
+        en:"Crops",
+        bn:'ব্যবহারকারীরা'
+    },
+    label_new_task:{
+        en:"New Crop",
+        bn:'নতুন ব্যবহারকারী'
+    },
+    label_edit_task:{
+        en:"Edit Crop",
+        bn:'সম্পাদনা ব্যবহারকারী'
+    },
+    label_details_task:{
+        en:"Details of Crop",
+        bn:'ব্যবহারকারীর বিবরণ'
+    },
+    label_code:{
+        en:"Code",
+        bn:"প্রিফিক্স"
+    },
+    label_replica :{
+        en:"Replica",
+        bn:"কর্মচারী আইডি"
+    },
+}
