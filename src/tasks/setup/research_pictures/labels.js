0f10795d8096a9c627f426992291da4bead0d092
Shaiful islam <islambuet@gmail.com>
2024-11-01 03:13:09 +0600

    Research Picture

---
+++ src/tasks/setup/research_pictures/labels.js
@@ -0,0 +1,18 @@
+export default {
+    label_task:{
+        en:"Research Picture Type",
+        bn:'ব্যবহারকারীরা'
+    },
+    label_new_task:{
+        en:"New Research Picture",
+        bn:'নতুন ব্যবহারকারী'
+    },
+    label_edit_task:{
+        en:"Edit Research Picture",
+        bn:'সম্পাদনা ব্যবহারকারী'
+    },
+    label_details_task:{
+        en:"Details of Research Picture",
+        bn:'ব্যবহারকারীর বিবরণ'
+    },
+}
