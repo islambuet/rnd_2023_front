104ec9725fffabc7b15b2e55d60ed14e4b6e2514
Shaiful islam <islambuet@gmail.com>
2024-11-01 02:37:55 +0600

    Crop Type2 included

---
+++ src/tasks/setup/crop-types2/labels.js
@@ -0,0 +1,30 @@
+export default {
+    label_task:{
+        en:"Crop Types(new)",
+        bn:'ব্যবহারকারীরা'
+    },
+    label_new_task:{
+        en:"New Type",
+        bn:'নতুন ব্যবহারকারী'
+    },
+    label_edit_task:{
+        en:"Edit Type",
+        bn:'সম্পাদনা ব্যবহারকারী'
+    },
+    label_details_task:{
+        en:"Details of Type",
+        bn:'ব্যবহারকারীর বিবরণ'
+    },
+    label_crop_id:{
+        en:"Crop",
+        bn:"কর্মচারী আইডি"
+    },
+    label_crop_name:{
+        en:"Crop",
+        bn:"কর্মচারী আইডি"
+    },
+    label_code:{
+        en:"Code",
+        bn:"প্রিফিক্স"
+    },
+}
