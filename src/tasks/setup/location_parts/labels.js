aa249d51479e2bf8fdc9227a5a2a1a58306184bd
Shaiful islam <islambuet@gmail.com>
2024-11-01 17:19:05 +0600

    Parts completed

---
+++ src/tasks/setup/location_parts/labels.js
@@ -0,0 +1,18 @@
+export default {
+    label_task:{
+        en:"Parts",
+        bn:'ব্যবহারকারীরা'
+    },
+    label_new_task:{
+        en:"New part",
+        bn:'নতুন ব্যবহারকারী'
+    },
+    label_edit_task:{
+        en:"Edit part",
+        bn:'সম্পাদনা ব্যবহারকারী'
+    },
+    label_details_task:{
+        en:"Details of part",
+        bn:'ব্যবহারকারীর বিবরণ'
+    },
+}
